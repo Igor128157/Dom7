3b471e8e07769ad6d80fc05a4dce3a0427158562
Igor Verovka <igor0063@ukr.net>
2020-04-03 15:35:16 +0300

    add

---
+++ task-04.js
@@ -0,0 +1,9 @@
+let counterValue = 0;
+function increment(){
+	counterValue +=1;
+  document.getElementById('value').innerHTML = counterValue;
+}
+function decrement(){
+	counterValue -=1;
+  document.getElementById('value').innerHTML = counterValue;
+}
